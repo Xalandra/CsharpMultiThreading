d6269d07dd19229bd2e8a442ca0c24c1efafef45
Michael Kes <michaelkes@outlook.com>
2017-03-15 13:07:17 +0100

    Pages toegevoegd alleen de download button werkt, en het menu is nog niet aan alle pages gekoppeld.

--- Vidarr/Vidarr/Convert.xaml.cs
+++ Vidarr/Vidarr/Convert.xaml.cs
@@ -13,28 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Vidarr
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Convert : Page
     {
-        public MainPage()
+        public Convert()
         {
             this.InitializeComponent();
-        }
-
-        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
-        {
-            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
-        }
-
-        private void MenuButton1_Click(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(Download));
         }
     }
 }
