9fa78fc3327b2c61d44234b3bc9923eeb91aee71
Michael Kes <michaelkes@outlook.com>
2017-04-11 13:52:09 +0200

    Design converter

--- Vidarr/Vidarr/pgConverter.xaml.cs
+++ Vidarr/Vidarr/pgConverter.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -32,6 +34,72 @@ namespace Vidarr
         {
             Convert conv = new Convert();
             conv.PickMedia();
+        }
+
+        private async void button1_Click(object sender, RoutedEventArgs e)
+        {
+            //Maak een nieuwe FileOpenPicker aan
+            FileOpenPicker openPicker = new FileOpenPicker();
+
+            //Kies welke weergave het moet hebben
+            openPicker.ViewMode = PickerViewMode.Thumbnail;
+
+            //STandaard openings plek
+            openPicker.SuggestedStartLocation = PickerLocationId.MusicLibrary;
+
+            //Bestands extensies die toegelaten worden
+            openPicker.FileTypeFilter.Add(".mp3");
+            openPicker.FileTypeFilter.Add(".mp4");
+            openPicker.FileTypeFilter.Add(".wma");
+            openPicker.FileTypeFilter.Add(".mov");
+            openPicker.FileTypeFilter.Add(".flv");
+            openPicker.FileTypeFilter.Add(".avi");
+
+            //Hier geven we de type selectie weer, single of multiple
+            StorageFile file = await openPicker.PickSingleFileAsync();
+            //MULTIPLE SELECTIE::: StorageFile file = await openPicker.PickMultipleFilesAsync();
+
+            if (file != null)
+            {
+                textBlock.Text = "Picked audio: " + file.Name;
+            }
+            else
+            {
+                //
+            }
+        }
+
+        private async void button2_Click(object sender, RoutedEventArgs e)
+        {
+            //Maak een nieuwe FileOpenPicker aan
+            FileOpenPicker openPicker = new FileOpenPicker();
+
+            //Kies welke weergave het moet hebben
+            openPicker.ViewMode = PickerViewMode.Thumbnail;
+
+            //STandaard openings plek
+            openPicker.SuggestedStartLocation = PickerLocationId.MusicLibrary;
+
+            //Bestands extensies die toegelaten worden
+            openPicker.FileTypeFilter.Add(".mp3");
+            openPicker.FileTypeFilter.Add(".mp4");
+            openPicker.FileTypeFilter.Add(".wma");
+            openPicker.FileTypeFilter.Add(".mov");
+            openPicker.FileTypeFilter.Add(".flv");
+            openPicker.FileTypeFilter.Add(".avi");
+
+            //Hier geven we de type selectie weer, single of multiple
+            StorageFile file = await openPicker.PickSingleFileAsync();
+            //MULTIPLE SELECTIE::: StorageFile file = await openPicker.PickMultipleFilesAsync();
+
+            if (file != null)
+            {
+                textBlock1.Text = "Picked audio: " + file.Name;
+            }
+            else
+            {
+                //
+            }
         }
     }
 }
