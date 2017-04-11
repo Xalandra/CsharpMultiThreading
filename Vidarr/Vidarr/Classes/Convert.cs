f80a3fa98fb87bfa565fbc77161aec05a00b5ad4
Xalandra <jannemarie.dekens@student.stenden.com>
2017-04-11 22:11:09 +0200

    methode aangepast zodat de picker niet meer een 2e, 3e of 4e keer nodig is voor user

--- Vidarr/Vidarr/Classes/Convert.cs
+++ Vidarr/Vidarr/Classes/Convert.cs
@@ -1,7 +1,12 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Internal;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Media.MediaProperties;
@@ -12,6 +17,8 @@ namespace Vidarr
 {
     public partial class Convert
     {
+        AsyncLock bouncer = new AsyncLock();
+
         void TranscodeProgress(IAsyncActionWithProgress<double> asyncInfo, double percent)
         {
             // Display or handle progress info.
@@ -88,6 +95,92 @@ namespace Vidarr
                         break;
                 }
             }
+        }
+
+        //[MethodImpl(MethodImplOptions.PreserveSig)]
+        public async void ConvertChosenMedia(List<StorageFile> selectedFiles, string output)
+        {
+            List<StorageFile> files = selectedFiles;
+            foreach (StorageFile file in files)
+            {
+                StorageFile source = file;
+                MediaEncodingProfile profile = new MediaEncodingProfile();
+
+                //wat wil de gebruiker er mee
+                switch (output)
+                {
+                    case "MP3":
+                        //savePicker.DefaultFileExtension = ".mp3";
+                        //savePicker.SuggestedFileName = "New Video";
+                        //savePicker.FileTypeChoices.Add("MPEG3", new string[] { ".mp3" });
+                        profile = MediaEncodingProfile.CreateMp3(AudioEncodingQuality.High);
+                        break;
+                    case "MP4":
+                        //savePicker.DefaultFileExtension = ".mp4";
+                        //savePicker.SuggestedFileName = "New Video";
+                        //savePicker.FileTypeChoices.Add("MPEG4", new string[] { ".mp4" });
+                        profile = MediaEncodingProfile.CreateMp4(VideoEncodingQuality.Auto);
+                        break;
+                    case "WAV":
+                        //savePicker.DefaultFileExtension = ".wav";
+                        //savePicker.SuggestedFileName = "New Video";
+                        //savePicker.FileTypeChoices.Add("WAV", new string[] { ".wav" });
+                        profile = MediaEncodingProfile.CreateWav(AudioEncodingQuality.High);
+                        break;
+                    default:
+                        Debug.WriteLine("Gekozen formaat om naar te converten staat niet in de switchlijst");
+                        break;
+
+                }
+
+                //bepaald waar het nieuwe bestand komt
+                var folder = KnownFolders.VideosLibrary;
+                string datetime = "_"+DateTime.Now.Day + DateTime.Now.Month + DateTime.Now.Year + DateTime.Now.Hour + DateTime.Now.Minute + DateTime.Now.Second;
+                var destination = await folder.CreateFileAsync(source.Name.Remove(source.Name.Length - 4)+ datetime+"." +output);
+
+                //StorageFile destination = await savePicker.PickSaveFileAsync();
+                MediaTranscoder transcoder = new MediaTranscoder();
+
+                PrepareTranscodeResult prepareOp = null;
+                while (source.Equals(null)|| destination.Equals(null)|| profile.Equals(null)) {
+                    //blijf wachten
+                }
+                using (await bouncer.LockAsync())
+                {
+                    //afvangen dat prepareOp nullreferenceexception gooit
+                    try
+                    {
+                        prepareOp = await transcoder.PrepareFileTranscodeAsync(source, destination, profile);
+                        //als het kan
+                        if (prepareOp.CanTranscode)
+                        {
+                            var transcodeOp = prepareOp.TranscodeAsync();
+
+                            transcodeOp.Progress +=
+                                new AsyncActionProgressHandler<double>(TranscodeProgress);
+                            transcodeOp.Completed +=
+                                new AsyncActionWithProgressCompletedHandler<double>(TranscodeComplete);
+                        }
+                        else
+                        {
+                            switch (prepareOp.FailureReason)
+                            {
+                                case TranscodeFailureReason.CodecNotFound:
+                                    Debug.WriteLine("Codec not found.");
+                                    break;
+                                case TranscodeFailureReason.InvalidProfile:
+                                    Debug.WriteLine("Invalid profile.");
+                                    break;
+                                default:
+                                    Debug.WriteLine("Unknown failure.");
+                                    break;
+                            }
+                        }
+                    }
+                    catch (NullReferenceException ex) { Debug.WriteLine(ex.Message); }
+                }
+            }//foreach
+            
         }
     }
 }
