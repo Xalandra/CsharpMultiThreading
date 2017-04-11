f809eb2482e4385234fcdcfe0eda6e2bb2910351
Rene Cordes <renecordes@hotmail.com>
2017-04-11 10:12:12 +0200

    oude code weggehaald

--- Vidarr/Vidarr/MainPage.xaml.cs
+++ Vidarr/Vidarr/MainPage.xaml.cs
@@ -97,64 +97,6 @@ namespace Vidarr
             watVanWebsite.Text = await ZoekZoekterm.crawlZoekterm(inputZoekterm.Text);
         }
 
-        //private async void Button_Click_1Async(object sender, RoutedEventArgs e)
-        //{
-        //    //MySqlConnection conn = new MySqlConnection("Server=127.0.0.1;Database=vidarr;Uid=root;Pwd='';SslMode=None;charset=utf8");
-        //    //MySqlCommand cmd = new MySqlCommand("SELECT * FROM video");
-
-        //    //conn.Open();
-        //    //MySqlDataReader data = cmd.ExecuteReader();
-        //    //while (data.Read())
-        //    //{
-        //    //    // doe iets met data
-        //    //    String row = data.GetString(0);
-        //    //}
-        //    //conn.Close();
-
-
-        //    //VERSIE CODE 2
-        //    //MySqlConnection sqlConnection1 = new MySqlConnection("Server=127.0.0.1;Database=vidarr;Uid=root;Pwd='';SslMode=None;charset=utf8");
-        //    //MySqlCommand cmd = new MySqlCommand();
-        //    //MySqlDataReader reader;
-
-        //    //cmd.CommandText = "SELECT * FROM video";
-        //    //cmd.CommandType = CommandType.Text;
-        //    //cmd.Connection = sqlConnection1;
-
-        //    //sqlConnection1.Open();
-
-        //    //reader = cmd.ExecuteReader();
-        //    //// Data is accessible through the DataReader object here.
-
-        //    //sqlConnection1.Close();
-
-        //    MySqlConnection conn;
-        //    string myConnectionString;
-
-        //    myConnectionString = "Server=127.0.0.1;Database=vidarr;Uid=root;Pwd='';SslMode=None;charset=utf8";
-
-        //    try
-        //    {
-        //        conn = new MySqlConnection(myConnectionString);
-        //        MySqlCommand cmd = new MySqlCommand();
-        //        MySqlDataReader reader;
-
-
-        //        cmd.CommandText = "SELECT * FROM video";
-        //        cmd.CommandType = CommandType.Text;
-        //        cmd.Connection = conn;
-        //        conn.Open();
-        //        reader = cmd.ExecuteReader();
-        //    }
-        //    catch (MySqlException ex)
-        //    {
-        //        //MessageBox.Show(ex.Message);
-
-        //        var dialog = new MessageDialog(ex.Message);
-        //        await dialog.ShowAsync();
-        //    }
-        //}
-
         private async void Button_Click_1Async(object sender, RoutedEventArgs e)
         {
             MySqlConnection conn;
